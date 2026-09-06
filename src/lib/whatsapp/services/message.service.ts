a7fef97b0c65a11f4af92c5d8678e16703306578
Aditya Raj Mishra <adityarajmishra807@gmail.com>
2026-09-06 21:13:06 +0530

    feat(whatsapp): expose media and contact sends

--- src/lib/whatsapp/services/message.service.ts
+++ src/lib/whatsapp/services/message.service.ts
@@ -4,12 +4,16 @@ import {
   getWhatsAppHistory,
   markWhatsAppMessagesRead,
   reactToWhatsAppMessage,
+  sendWhatsAppContact,
+  sendWhatsAppMedia,
   sendWhatsAppMessage,
 } from "../core/api.server";
 
 export const messageService = {
   history: getWhatsAppHistory,
   sendText: sendWhatsAppMessage,
+  sendMedia: sendWhatsAppMedia,
+  sendContact: sendWhatsAppContact,
   markRead: markWhatsAppMessagesRead,
   edit: editWhatsAppMessage,
   remove: deleteWhatsAppMessage,
