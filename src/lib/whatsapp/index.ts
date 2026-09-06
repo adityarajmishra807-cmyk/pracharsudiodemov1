b143b02d0c931d4d25781588fdbe954ae31f0b2f
adityarajmishra807-cmyk <adityarajmishra807@gmail.com>
2026-09-06 14:31:51 +0530

    Add structured WhatsApp module exports

---
+++ src/lib/whatsapp/index.ts
@@ -0,0 +1,8 @@
+export * from "./core/types";
+export * from "./core/normalizers";
+export * from "./hooks/useWhatsAppSessions";
+export * from "./hooks/useWhatsAppChats";
+export * from "./hooks/useWhatsAppMessages";
+export { sessionService } from "./services/session.service";
+export { chatService } from "./services/chat.service";
+export { messageService } from "./services/message.service";
