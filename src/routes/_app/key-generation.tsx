244cb9e6d07f62e785d6b15d82ce4f11c205aa16
Aditya Raj Mishra <adityarajmishra807@gmail.com>
2026-09-23 17:31:43 +0530

    refactor: use client-callable server function module

--- src/routes/_app/key-generation.tsx
+++ src/routes/_app/key-generation.tsx
@@ -31,7 +31,7 @@ import {
   listLicensesFn,
   revokeLicenseFn,
 } from "@/lib/server-license";
-import { getAuthFn } from "@/server/auth";
+import { getAuthFn } from "@/lib/server-auth";
 import type { LicenseRow } from "@/lib/server-license-store";
 
 export const Route = createFileRoute("/_app/key-generation")({
